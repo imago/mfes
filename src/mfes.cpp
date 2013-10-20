55d2ed15d04cf21fd5d532a4edaac812efb7bef2
imago <sakalli@chemie.fu-berlin.de>
2013-10-20 13:29:26 -0700

    * initial commit

---
+++ src/mfes.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : mfes.cpp
+ Author      : Ilkay Sakalli
+ Version     :
+ Copyright   : GNU Library or Lesser General Public License version 2.0 (LGPLv2)
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World from mFES" << endl; /* prints Hello World from mFES */
+	return 0;
+}
